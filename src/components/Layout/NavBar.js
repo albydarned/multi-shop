4828ba13d99889c56b1b2dd636fb2414b1f7ffdd
Steve Albrechtsen <steven.albrechtsen@gmail.com>
2022-10-19 22:05:00 -0600

    i made a change

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
                         <a href="/" className="text-decoration-none d-block d-lg-none">
                             <span className="h1 text-uppercase text-dark bg-light px-2">Multi</span>
                             <span className="h1 text-uppercase text-light bg-primary px-2 ml-n1">Shop</span>
-                        </a>
+                        </a>asdf
                         <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                             <span className="navbar-toggler-icon"></span>
                         </button>
