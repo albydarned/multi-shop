db2c239f5ae02b512894530cb515a20f69593cb3
Steve Albrechtsen <steven.albrechtsen@gmail.com>
2022-10-25 23:20:08 -0600

    context loading categories

--- src/store/product-context.js
+++ src/store/product-context.js
@@ -10,10 +10,28 @@ export const ProductContextProvider = (props) => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        getCategories();
+        init();
     }, []);
 
-    const getCategories = async () => {
+    const init = async () => {
+        // Load the categories from the window session
+        let sessionCategoriesJSON = window.sessionStorage.getItem("categories");
+
+        // If the window categories are not set, we need to load them
+        if (sessionCategoriesJSON == null) {
+            // Wait until the categories are loaded from the API
+            await loadCategories();
+            // Save the loaded categories to the window session
+            window.sessionStorage.setItem("categories", JSON.stringify(categories));
+        
+        // If the window categories are set, parse the json and save to state
+        } else {
+            let sessionCategories = JSON.parse(sessionCategoriesJSON);
+            setCategories(sessionCategories);
+        }
+    }
+
+    const loadCategories = async () => {
         try {
             const result = await axios.get('https://dummyjson.com/products?limit=100');
             let newCategoryList = {};
