dcbcdb5e575193e539e646e4e6910fa603fc7a91
Steve Albrechtsen <steven.albrechtsen@gmail.com>
2022-10-25 21:59:43 -0600

    add sidebar

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -1,16 +1,23 @@
 import ProductTile from './ProductTile';
+import ProductSidebar from './ProductSidebar';
 
 const ProductList = (props) => {
 
     return (
-        <div className={props.rowClasses}>
-            {
-                props.products.map((product) => {
-                    return (
-                        <ProductTile product={product} classes={props.productClasses} key={product.id} />
-                    )
-                })
-            }
+        <div>
+            <div>
+                <ProductSidebar />
+            </div>
+
+            <div className={props.rowClasses}>
+                {
+                    props.products.map((product) => {
+                        return (
+                            <ProductTile product={product} classes={props.productClasses} key={product.id} />
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
